0108e1248243c15642b3bc741bb503bea1c73131
Jomari Hinayon <hinayonjomari@gmail.com>
2024-01-24 16:22:43 +0800

    Integrate drf-spectacular and swagger for documentation

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     #external apps
     'rest_framework',
     'mptt',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -110,5 +111,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'DRF E-commerce API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
