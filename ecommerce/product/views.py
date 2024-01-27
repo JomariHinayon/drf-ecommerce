2d7667f7645fc01bcbc5e9db834e2ebe81ec0c99
Jomari Hinayon <hinayonjomari@gmail.com>
2024-01-27 10:49:16 +0800

    Add new endpoint for getting products by category

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import action
 
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 from .models import Category, Brand, Product
@@ -21,7 +22,7 @@ class CategoryViewSet(viewsets.ViewSet):
 
 class ProductViewSet(viewsets.ViewSet):
     """
-    A simple Viewset for viewing all products
+        A simple Viewset for viewing all products
     """
 
     queryset = Product.objects.all()
@@ -30,9 +31,18 @@ class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
+    
+    """
+        Endpoint for getting the products by category
+    """
+    @action(methods=["get"], detail=False, url_path=r"category/(?P<category>\w+)/all")
+    def list_product_by_category(self, request, category=None):
+        serializer = ProductSerializer(self.queryset.filter(category__name=category), many=True)
+        return Response(serializer.data)
+
 
 class BrandViewSet(viewsets.ViewSet):
-    """
+    """.
     A simple Viewset for viewing all brands
     """
 
